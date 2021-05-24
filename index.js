3327a046eee46d98f978d276d720bbc8d88235df
Lucas Piputto <lucaspiputto@gmail.com>
2021-05-24 20:48:15 -0300

    agrega Morgan

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const express = require('express');
+const morgan  = require('morgan');
 const app = express();
-
-const f = (req, res, next) => {
-    console.log('voy primero');
-    next()
-};
-const g = (req, res, next) => {
-  console.log('voy segundo');
-  next();
-};
-const h = (req, res, next) => {
-  res.end();
-};
-  
-let middleWares = [f, g, h];
-
-// app.get('/hola', (req, res) => {
-//     console.log('Hola');
-//     res.end();
-// });
-app.get('/hola', middleWares);
+app.use(morgan('dev'));
+app.get('/hola', (req, res) => {
+    res.send('Hola');
+});
 app.listen(3000);
